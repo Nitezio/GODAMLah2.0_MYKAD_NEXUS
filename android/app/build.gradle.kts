69267f5fc544d3f7bf1561211a3764801bc25c54
NITESH A/L PRAKASH <nitesh0605@gmail.com>
2025-12-22 01:57:50 +0800

    working with login and new updated plan on emulator

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,15 +3,16 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-
-    // --- ADD THIS LINE BELOW ---
     id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.example.godamlah_v2_mykad_nexus"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+
+    // --- THIS IS THE FIX ---
+    ndkVersion = "27.0.12077973"
+    // ----------------------
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -23,13 +24,10 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.godamlah_v2_mykad_nexus"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
 
-        // CHANGE THIS LINE BELOW FROM flutter.minSdkVersion TO 21
-        minSdk = flutter.minSdkVersion
+        // Ensure minSdkVersion is at least 21 for modern plugins
+        minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -37,8 +35,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
